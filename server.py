72af1b8d6d4277afb7b767e5288f9909d379e40a
David Hui <opensource@davidhui.ca>
2019-04-30 15:52:53 -0400

    light commenting

--- server.py
+++ server.py
@@ -19,8 +19,13 @@ fp.loadSettings()
 
 fileIndex = fi.Index(db=MONGO_STRING)
 
+# Create a thread pool so that Python doesn't spawn thousands of threads during indexing
 tp =  ThreadPool(16)
+
 exclusions = ["node_modules", "\.git"]
+
+# Compile all the user's expressions
+# TODO: ERROR HANDLING
 exclusionsCompiled = [re.compile(exp) for exp in exclusions]
 
 def populateProperties(path, options={}):
@@ -33,36 +38,51 @@ def populateProperties(path, options={}):
 def createIndex(path, options={}):
     allPaths = []
     if os.path.isdir(path):
+        # Create a path queue and a depth queue to keep track of node depth
         fqueue = queue.Queue()
         dqueue = queue.Queue()
+
+        # Add the root path and a depth of 0
         fqueue.put(path)
         dqueue.put(0)
+
+        # Keep running until the queue has been emptied
         while not fqueue.empty():
+            # Get the path of the item and its depth
             item = fqueue.get()
             cdepth = dqueue.get()
+            # Check if maximum recursion depth has been reached
             if cdepth <= MAX_RECURSION_DEPTH:
                 children = os.listdir(item)
+                # Loop through children (make sure the file/directory still exists before performing action)
                 for child in children:
                     cfullpath = os.path.join(item,child)
-                    if os.path.isdir(cfullpath):
+                    if os.path.exists(cfullpath):
+                        # Loop through the exclusions to check if we need to even process the file
                         excluded = False
                         for exclu in exclusionsCompiled:
                             if exclu.search(cfullpath):
                                 excluded = True
                                 break
                         if not excluded:
-                            fqueue.put(os.path.join(item,child))
-                            dqueue.put(cdepth+1)
+                            # If it is a directory, add it to the back of the queue
+                            if os.path.isdir(cfullpath):
+                                fqueue.put(os.path.join(item,child))
+                                dqueue.put(cdepth+1)
+                            # Add to the list of files/directories to be processed
                             allPaths.append(os.path.join(item,child))
             else:
                 break
                     
         for path in allPaths:
+            # Assign a new task to populate properties of the file/directory to the thread pool
             tp.apply_async(populateProperties, args=(path,))
+            
+        # Cleanup thread pool
         tp.close()
         tp.join()
     else:
-        return fi.Response(400, "The path specified does not exist")
+        return fi.Response(400, "The path specified does not exist or is not a directory")
 
 def loadSettings():
     fp.loadSettings()
